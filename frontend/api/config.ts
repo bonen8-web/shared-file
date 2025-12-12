daaf0b699624bc630883a576f3928ce605f5ff93
inbarle2 <inbar.leshem@gmail.com>
2025-12-12 19:41:32 +0200

    Added dist folder for cPanel deployment

--- frontend/api/config.ts
+++ frontend/api/config.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // כתובת השרת המרכזית - שנה כאן אם הכתובת משתנה
-const API_BASE_URL = 'http://192.162.2.148:5000/api';
-//const API_BASE_URL = 'https://pettime8.onrender.com/api';
+//const API_BASE_URL = 'http://192.162.2.148:5000/api';
+const API_BASE_URL = 'https://pettime8.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
