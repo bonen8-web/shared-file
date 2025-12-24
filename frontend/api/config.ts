a684970d41df5b617c38990a8c56a0bf1afc6d95
inbarle2 <inbar.leshem@gmail.com>
2025-12-24 19:17:32 +0200

    Change API URL to HTTPS

--- frontend/api/config.ts
+++ frontend/api/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // כתובת השרת המרכזית - שנה כאן אם הכתובת משתנה
-const API_BASE_URL = 'http://orelbo2.mtacloud.co.il';
+const API_BASE_URL = 'https://orelbo2.mtacloud.co.il';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
